f10fe150f696c46f44f761c356066609cbd609de
ylevanon <yoavlevanoni@outlook.com>
2024-01-21 23:27:00 -0800

    added pg

--- project/database.py
+++ project/database.py
@@ -4,6 +4,5 @@ from . import app
 
 DATABASE_URL = os.environ["DATABASE_URL"]
 
-if __name__ == "__main__":
-    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
-    db = SQLAlchemy(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
+db = SQLAlchemy(app)
