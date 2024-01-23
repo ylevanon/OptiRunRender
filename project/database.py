2b9291e3cc4e2903295e74cb6ee8242fcdf5f6cc
ylevanon <yoavlevanoni@outlook.com>
2024-01-22 16:08:43 -0800

    added pg

--- project/database.py
+++ project/database.py
@@ -8,6 +8,6 @@ database_url = os.environ["DATABASE_URL"]
 # Check if the URL starts with postgres:// and replace it with postgresql://
 if database_url.startswith("postgres://"):
     database_url = database_url.replace("postgres://", "postgresql://", 1)
-
+print(database_url)
 app.config["SQLALCHEMY_DATABASE_URI"] = database_url
 db = SQLAlchemy(app)
