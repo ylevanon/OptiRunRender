bcc8acbae376bae4120875588147f1cb9b7864df
ylevanon <yoavlevanoni@outlook.com>
2024-01-22 20:03:03 -0800

    routes in db

--- project/__init__.py
+++ project/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
-from flask_migrate import Migrate
 
 
 from .extensions import db
@@ -13,13 +12,12 @@ from .views import main
 def create_app():
     app = Flask(__name__)
 
-    database_url = os.environ["DATABASE_URL"]
+    database_url = os.environ.get("DATABASE_URL")
     if database_url.startswith("postgres://"):
         database_url = database_url.replace("postgres://", "postgresql://", 1)
 
     app.config["SQLALCHEMY_DATABASE_URI"] = database_url
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
-    migrate = Migrate(app, db)
     app.register_blueprint(main)
     return app
