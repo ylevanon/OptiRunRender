b86e6a41c650faad49c7e2c07f62ed5ce6811eae
ylevanon <yoavlevanoni@outlook.com>
2024-01-27 16:51:36 -0800

    initial user auth UI

--- project/views.py
+++ project/views.py
@@ -2,7 +2,7 @@ from flask import Blueprint, abort, render_template, redirect, request, url_for
 from rq import Queue
 from flask import render_template, flash, redirect, url_for
 from flask_login import current_user, login_user, logout_user, login_required
-from werkzeug.urls import url_parse
+from urllib.parse import urlparse
 from .forms import LoginForm, RegistrationForm
 from .models import User
 from .models import Route
@@ -24,7 +24,7 @@ def login():
             return redirect(url_for("main.login"))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get("next")
-        if not next_page or url_parse(next_page).netloc != "":
+        if not next_page or urlparse(next_page).netloc != "":
             next_page = url_for("main.index")
         return redirect(next_page)
     return render_template("login.html", title="Sign In", form=form)
