c4a509d2bb333189974e55f7a336740ed801e5b3
ylevanon <yoavlevanoni@outlook.com>
2024-01-27 17:23:44 -0800

    added secret key

--- project/views.py
+++ project/views.py
@@ -45,7 +45,7 @@ def index():
         db.session.add(user)
         db.session.commit()
         flash("Congratulations, you are now a registered user!")
-        return redirect(url_for("main.login"))
+        return redirect(url_for("main.landing"))
 
     return render_template(
         "login_register.html", login_form=login_form, register_form=register_form
@@ -53,20 +53,20 @@ def index():
 
     # @main.route("/login", methods=["GET", "POST"])
     # def login():
-    if current_user.is_authenticated:
-        return redirect(url_for("main.index"))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
-            flash("Invalid username or password")
-            return redirect(url_for("main.login"))
-        login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get("next")
-        if not next_page or urlparse(next_page).netloc != "":
-            next_page = url_for("main.index")
-        return redirect(next_page)
-    return render_template("login.html", title="Sign In", form=form)
+    # if current_user.is_authenticated:
+    #     return redirect(url_for("main.index"))
+    # form = LoginForm()
+    # if form.validate_on_submit():
+    #     user = User.query.filter_by(username=form.username.data).first()
+    #     if user is None or not user.check_password(form.password.data):
+    #         flash("Invalid username or password")
+    #         return redirect(url_for("main.login"))
+    #     login_user(user, remember=form.remember_me.data)
+    #     next_page = request.args.get("next")
+    #     if not next_page or urlparse(next_page).netloc != "":
+    #         next_page = url_for("main.index")
+    #     return redirect(next_page)
+    # return render_template("login.html", title="Sign In", form=form)
 
 
 @main.route("/logout")
@@ -76,19 +76,19 @@ def logout():
 
     # @main.route("/register", methods=["GET", "POST"])
     # def register():
-    if current_user.is_authenticated:
-        return redirect(url_for("main.index"))
-    from .extensions import db
+    # if current_user.is_authenticated:
+    #     return redirect(url_for("main.index"))
+    # from .extensions import db
 
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data)
-        user.set_password(form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        flash("Congratulations, you are now a registered user!")
-        return redirect(url_for("main.login"))
-    return render_template("register.html", title="Register", form=form)
+    # form = RegistrationForm()
+    # if form.validate_on_submit():
+    #     user = User(username=form.username.data, email=form.email.data)
+    #     user.set_password(form.password.data)
+    #     db.session.add(user)
+    #     db.session.commit()
+    #     flash("Congratulations, you are now a registered user!")
+    #     return redirect(url_for("main.login"))
+    # return render_template("register.html", title="Register", form=form)
 
 
 @main.route("/about")
