4b97e1fcbabc9ca32de649a0b4a34be837306163
ylevanon <yoavlevanoni@outlook.com>
2024-02-19 11:43:21 -0800

    user add debug

--- project/forms.py
+++ project/forms.py
@@ -1,12 +1,13 @@
 # forms.py
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 
 class LoginForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
+    remember_me = BooleanField("Remember Me")
     submit = SubmitField("Login")
 
 
