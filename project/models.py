cda928582e621ac15e4d914cbd13cc0cf090134b
ylevanon <yoavlevanoni@outlook.com>
2024-02-19 12:41:39 -0800

    only users can view their routes

--- project/models.py
+++ project/models.py
@@ -12,8 +12,6 @@ class User(UserMixin, db.Model):
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
-        print("hash password")
-        print(self.password_hash)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
--- project/views.py
+++ project/views.py
@@ -64,44 +64,11 @@ def index():
         "login_register.html", login_form=login_form, register_form=register_form
     )
 
-    # @main.route("/login", methods=["GET", "POST"])
-    # def login():
-    # if current_user.is_authenticated:
-    #     return redirect(url_for("main.index"))
-    # form = LoginForm()
-    # if form.validate_on_submit():
-    #     user = User.query.filter_by(username=form.username.data).first()
-    #     if user is None or not user.check_password(form.password.data):
-    #         flash("Invalid username or password")
-    #         return redirect(url_for("main.login"))
-    #     login_user(user, remember=form.remember_me.data)
-    #     next_page = request.args.get("next")
-    #     if not next_page or urlparse(next_page).netloc != "":
-    #         next_page = url_for("main.index")
-    #     return redirect(next_page)
-    # return render_template("login.html", title="Sign In", form=form)
-
 
 @main.route("/logout")
 def logout():
     logout_user()
     return redirect(url_for("main.index"))
-
-    # @main.route("/register", methods=["GET", "POST"])
-    # def register():
-    # if current_user.is_authenticated:
-    #     return redirect(url_for("main.index"))
-    # from .extensions import db
-
-    # form = RegistrationForm()
-    # if form.validate_on_submit():
-    #     user = User(username=form.username.data, email=form.email.data)
-    #     user.set_password(form.password.data)
-    #     db.session.add(user)
-    #     db.session.commit()
-    #     flash("Congratulations, you are now a registered user!")
-    #     return redirect(url_for("main.login"))
-    # return render_template("register.html", title="Register", form=form)
 
 
 @main.route("/about")
@@ -139,6 +106,7 @@ def loading(task_id):
         return render_template("error.html")
 
 
+@login_required
 @main.route("/customized_run/<route_id>")
 def customized_run(route_id):
     # Query the database for the Route with the provided ID
